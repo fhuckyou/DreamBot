1c5d23d765a3dd84513247d0e61024e14ca282b9
Fhuckyou <Fhuckyou@10.0.0.10>
2016-01-07 23:23:46 -0500

    Updates

--- RiskyYews/src/main/RiskyYews.java
+++ RiskyYews/src/main/RiskyYews.java
@@ -8,6 +8,7 @@ import java.text.DecimalFormat;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.map.Tile;
 import org.dreambot.api.methods.skills.Skill;
+import org.dreambot.api.methods.tabs.Tab;
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
@@ -15,6 +16,8 @@ import org.dreambot.api.script.listener.PaintListener;
 import org.dreambot.api.utilities.Timer;
 import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.wrappers.widgets.message.Message;
+
+import util.Variables;
 
 @ScriptManifest(author = "Fhuckyou", category = Category.WOODCUTTING, name = "RiskyYews", version = 1.0)
 public class RiskyYews extends AbstractScript implements PaintListener {
@@ -26,37 +29,54 @@ public class RiskyYews extends AbstractScript implements PaintListener {
 	private int itemsMade;
 	private double gpGained;
 	private double totalGpGained;
+	
+	private GameObject eastYew;
+	private GameObject westYew;
+	private final Tile tt = new Tile(2997,3314,0);
+	private final Tile t = new Tile(3022,3315,0);
+	private String i = "Yew";
+	private String currentState = "";
 
 	
 	
 	private enum State{
-        CHOP, WAITING, RUN, BANK, WALKTOYEW;//states
+        CHOP, WAITING, RUN, BANK, WALKTOYEW, WALKTOYEW2;//states
 }
 	
 	private State getState(){
+		eastYew = getGameObjects().closest(yew -> yew != null
+				&& yew.getName() != null
+				&& yew.getName().equals(i)
+				&& yew.distance(tt) < 8
+				&& !getLocalPlayer().isAnimating()
+				&& !getLocalPlayer().isInCombat());
 		
-		currentYew = getGameObjects().closest(yew -> yew != null
+		westYew = getGameObjects().closest(yew -> yew != null
 				&& yew.getName() != null
 				&& yew.getName().equals(i)
 				&& yew.distance(t) < 5
 				&& !getLocalPlayer().isAnimating()
 				&& !getLocalPlayer().isInCombat());
 		
-		if(currentYew == null && !getInventory().isFull()
-				&& getInventory().contains("Steel axe")
+		if(westYew == null && eastYew != null
+				&& !getInventory().isFull()
 				//&& getInventory().contains("Cake")
+				&& getInventory().contains("Steel axe")
 				&& !getLocalPlayer().isAnimating()
-				&& getLocalPlayer().distance(t) > 25) {
-			return state.WALKTOYEW;
+				&& getLocalPlayer().distance(t) < 8) {
+			log("in 2");
+			return state.WALKTOYEW2;
 		}
 		
-		if(currentYew != null && !getInventory().isFull()
-				&& getInventory().contains("Steel axe")
+		if(westYew != null && eastYew == null
+				&& !getInventory().isFull()
 				//&& getInventory().contains("Cake")
+				&& getInventory().contains("Steel axe")
 				&& !getLocalPlayer().isAnimating()
-				&& getLocalPlayer().distance(t) > 25) {
-			return state.WALKTOYEW;		
-			}
+				&& getLocalPlayer().distance(tt) < 8) {
+			log("in 1");
+			return state.WALKTOYEW;
+		}
 		
 		
 		
@@ -69,17 +89,25 @@ public class RiskyYews extends AbstractScript implements PaintListener {
 			return state.RUN;
 		}
 		
-		if(currentYew != null && !getInventory().isFull()
+		if(westYew != null && !getInventory().isFull()
+				//&& getInventory().contains("Cake")
+				&& getInventory().contains("Steel axe")
+				|| eastYew != null && !getInventory().isFull()
+						//&& getInventory().contains("Cake")
+						&& getInventory().contains("Steel axe")) {
+			return state.CHOP;
+		}
+		
+		if(westYew != null || eastYew != null) {
+			if(!getInventory().isFull()
 				//&& getInventory().contains("Cake")
 				&& getInventory().contains("Steel axe")) {
 			return state.CHOP;
 		}
+		}
 		return state.WAITING;
 	}
-	private GameObject currentYew;
-	private final Tile t = new Tile(3022,3315,0);
 	private final Tile saftey = new Tile(3007,3339,0);
-	private String i = "Yew";
 	
 	public void onMessage(Message m) {
 		if (m.getMessage().contains("You get some yew logs.")) {
@@ -94,15 +122,35 @@ public class RiskyYews extends AbstractScript implements PaintListener {
 		switch(state) {
 		
 		case CHOP:
-			if(currentYew.interact("Chop down")) {
-				sleepUntil(() -> !getLocalPlayer().isAnimating(), 2000);
-				log("Potential tree to cut" + currentYew.getName());
+			if(getLocalPlayer().distance(eastYew) < getLocalPlayer().distance(westYew)) {
+				getWalking().walk(eastYew);
+				sleepUntil(() -> eastYew.isOnScreen(), 2000);
+				if(eastYew != null && eastYew.isOnScreen()) {
+					eastYew.interact("Chop down");
+					sleep(500);
+					sleepUntil(() -> !getLocalPlayer().isAnimating(), 2000);
+					log("Potential tree to cut" + westYew.getName());
+				}
+			} else {
+				getWalking().walk(westYew);
+				sleepUntil(() -> westYew.isOnScreen(), 2000);
+				if(westYew != null && westYew.isOnScreen()) {
+					westYew.interact("Chop down");
+					sleep(500);
+					sleepUntil(() -> !getLocalPlayer().isAnimating(), 2000);
+					log("Potential tree to cut" + westYew.getName());
+				}
 			}
 			break;
 			
 		case WALKTOYEW:
 			log("walking");
 				getWalking().walk(t);
+			break;
+			
+		case WALKTOYEW2:
+			log("walking2");
+				getWalking().walk(tt);
 			break;
 			
 		case BANK:
@@ -132,9 +180,17 @@ public class RiskyYews extends AbstractScript implements PaintListener {
 					getWalking().toggleRun();
 				}
 				getWalking().walk(saftey);
+				
+				if(!getTabs().isOpen(Tab.LOGOUT) && !getLocalPlayer().isInCombat()) {
+					getTabs().logout();
+				} else if(getTabs().isOpen(Tab.LOGOUT) && !getLocalPlayer().isInCombat()){
+					getTabs().logout();
+				}
+				sleep(13000);
 			break;
 			
 		case WAITING:
+			currentState = "Chopping yew tree..";
 			log("Waiting.");
 			break;
 			
@@ -154,7 +210,7 @@ public class RiskyYews extends AbstractScript implements PaintListener {
 		//DecimalFormat df = new DecimalFormat("#");
 		//g.drawString("Total $ earned:"+ df.format(totalGpGained) + "Gp ", 250, 396);
 		if(state != null)
-			g.drawString("State: " + state.toString(), 250, 396);//409
+			g.drawString("State: " + currentState.toString(), 250, 396);//409
 	}
 	
 	public void onStart() {
--- RiskyYews/src/util/Variables.java
+++ RiskyYews/src/util/Variables.java
@@ -1,5 +1,14 @@
 package util;
 
-public class Variables {
+import org.dreambot.api.methods.map.Tile;
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.wrappers.interactive.GameObject;
 
+public class Variables {
+	
+	public static GameObject eastYew;
+	public static GameObject westYew;
+	public static final Tile t = new Tile(3022,3315,0);
+	public static String i = "Yew";
 }
+
